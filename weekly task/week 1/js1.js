7002f5d71c3c24309a4a4f2645821a2bfd04ae77
unknown <raissarahmani18@gmail.com>
2024-11-29 10:35:36 +0700

    weekly task submit

---
+++ weekly task/week 1/js1.js
@@ -0,0 +1,15 @@
+let item = {
+    name: "Sketchbook",
+    rating: 5,
+    price: 50000,
+    isActive: true,
+    images: ["good quality", "tebal", "hard cover"],
+    description: "Sketchbook berkualitas sangat baik untuk mendesain dan menggambar dengan detail",
+    variants: {
+        name: "Sketchbook",
+        options: ["A4", "A5"],
+    },
+    storeName: "Artmedia Bookstore"
+}
+
+console.log(item)
---
+++ weekly task/week 1/js4.js
@@ -0,0 +1,18 @@
+// 4a. gabungkan dengan spread operator //
+const arr1 = [1,2,3]
+const arr2 = [4,5,6]
+let newArray = [...arr1, ...arr2]
+console.log(newArray)
+
+// 4b. tambahkan dengan spread operator //
+const person = {
+    name: "John",
+    age: 30
+}
+let newPerson = {
+    ...person,
+    gender: "male",
+    job: "pilot"
+}
+
+console.log(newPerson)
